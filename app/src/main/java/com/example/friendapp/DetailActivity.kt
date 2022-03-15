fb5ced285feee5648791a1db3f440afb17f70be0
EmilseBilse <emil875b@easv365.dk>
2022-03-15 18:51:32 +0100

    nakket oles kode

--- app/src/main/java/com/example/friendapp/DetailActivity.kt
+++ app/src/main/java/com/example/friendapp/DetailActivity.kt
@@ -1,11 +1,18 @@
 package com.example.friendapp
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import android.Manifest
 
 private const val TAG = "detailActivity"
 
@@ -26,19 +33,25 @@ class DetailActivity : AppCompatActivity() {
         if(!isCreateMenu){
             name.setText(friends.getAll()[friend].name)
             phone.setText(friends.getAll()[friend].phone)
+            email.setText(friends.getAll()[friend].email)
+            url.setText(friends.getAll()[friend].url)
         }
 
 
         btnDelete.setOnClickListener { onClickDelete()}
         btnDetailsBack.setOnClickListener{ finish() }
         btnSave.setOnClickListener { onClickSave() }
+        btnCallFriend.setOnClickListener{ onClickCall() }
+        btnGoToFriendUrl.setOnClickListener { onClickBrowser() }
+        btnEmailFriend.setOnClickListener { onClickEmail() }
+        btnSMSFriend.setOnClickListener { onClickSms() }
     }
 
     private fun onClickSave() {
         if(isCreateMenu) {
-            friends.addFriend(BEFriend(name.text.toString(), phone.text.toString()))
+            friends.addFriend(BEFriend(name.text.toString(), phone.text.toString(), "", ""))
         }else{
-            friends.updateFriend(friend,BEFriend(name.text.toString(), phone.text.toString()))
+            friends.updateFriend(friend,BEFriend(name.text.toString(), phone.text.toString(), "", ""))
         }
 
         val data = Intent().apply { putExtra("friendListUpdated", friends) }
@@ -56,4 +69,83 @@ class DetailActivity : AppCompatActivity() {
         setResult(Activity.RESULT_OK, data)
         finish()
     }
+
+    private fun onClickCall() {
+        var number = friends.getAll()[friend].phone
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:$number")
+        startActivity(intent)
+    }
+
+    private fun onClickBrowser() {
+        val url = friends.getAll()[friend].url
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
+
+    private fun onClickEmail() {
+        Log.d(TAG, "onClickEMAIL started")
+        val emailIntent = Intent(Intent.ACTION_SEND)
+        emailIntent.type = "plain/text"
+        val receivers = friends.getAll()[friend].email
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, receivers)
+        Log.d(TAG, "onClickEMAIL: intent preprared")
+
+        startActivity(emailIntent)
+        Log.d(TAG, "Email activity started")
+    }
+
+    private fun onClickSms() {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("SMS Handling")
+        alertDialogBuilder
+            .setMessage("Click Direct if SMS should be send directly. Click Start to start SMS app...")
+            .setCancelable(true)
+            .setPositiveButton("Direct") { dialog, id -> sendSMSDirectly() }
+            .setNegativeButton("Start") { dialog, id -> startSMSActivity() }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
+    private fun startSMSActivity() {
+        val sendIntent = Intent(Intent.ACTION_VIEW)
+        var number = friends.getAll()[friend].phone
+        sendIntent.data = Uri.parse("sms:$number")
+        startActivity(sendIntent)
+    }
+
+    val PERMISSION_REQUEST_CODE = 1
+
+    private fun sendSMSDirectly() {
+        Toast.makeText(this, "An sms will be send", Toast.LENGTH_LONG)
+            .show()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.SEND_SMS)
+                == PackageManager.PERMISSION_DENIED) {
+                Log.d(TAG, "permission denied to SEND_SMS - requesting it")
+                val permissions = arrayOf(Manifest.permission.SEND_SMS)
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE)
+                return
+            } else Log.d(TAG, "permission to SEND_SMS granted!")
+        } else Log.d(TAG, "Runtime permission not needed")
+        sendMessage()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        Log.d(TAG, "Permission: " + permissions[0] + " - grantResult: " + grantResults[0])
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            sendMessage()
+        }
+    }
+
+    private fun sendMessage() {
+        val m = SmsManager.getDefault()
+        var number = friends.getAll()[friend].phone
+        m.sendTextMessage(number, null, null, null, null)
+    }
+
 }
--- app/src/main/java/com/example/friendapp/Friends.kt
+++ app/src/main/java/com/example/friendapp/Friends.kt
@@ -5,27 +5,27 @@ import java.io.Serializable
 class Friends : Serializable{
 
     val mFriends = arrayOf<BEFriend>(
-        BEFriend("Simon", "123"),
-        BEFriend("Dennis", "1234"),
-        BEFriend("Mina", "12345"),
-        BEFriend("Emil", "12345678"),
-        BEFriend("Mads", "23456789"),
-        BEFriend("Martin", "87654321"),
-        BEFriend("Mike", "12121212"),
-        BEFriend("Trine", "123"),
-        BEFriend("Mathias", "1234"),
-        BEFriend("Rasmus", "12345"),
-        BEFriend("Christian", "12345678"),
-        BEFriend("Peter", "88888888"),
-        BEFriend("Anders", "87654321"),
-        BEFriend("Mikkel", "12121212"),
-        BEFriend("Flemming", "123"),
-        BEFriend("Jonas", "1234"),
-        BEFriend("Frederik", "12345"),
-        BEFriend("Mantas", "12345678"),
-        BEFriend("Michael", "23456789"),
-        BEFriend("Jens", "87654321"),
-        BEFriend("Jan", "12121212")
+        BEFriend("Simon", "123", "Simon@Simon.com", "https://www.Simon.com"),
+        BEFriend("Dennis", "1234", "Dennis@Dennis.com", "https://www.Dennis.com"),
+        BEFriend("Mina", "12345", "Mina@Mina.com", "https://www.Mina.com"),
+        BEFriend("Emil", "12345678", "Emil@Emil.com", "https://www.Emil.com"),
+        BEFriend("Mads", "23456789", "Mads@Mads.com", "https://www.Mads.com"),
+        BEFriend("Martin", "87654321", "Martin@Martin.com", "https://www.Martin.com"),
+        BEFriend("Mike", "12121212", "Mike@Mike.com", "https://www.Mike.com"),
+        BEFriend("Trine", "123", "Trine@Trine.com", "https://www.Trine.com"),
+        BEFriend("Mathias", "1234", "Mathias@Mathias.com", "https://www.Mathias.com"),
+        BEFriend("Rasmus", "12345", "Rasmus@Rasmus.com", "https://www.Rasmus.com"),
+        BEFriend("Christian", "12345678", "Christian@Christian.com", "https://www.Christian.com"),
+        BEFriend("Peter", "88888888", "Peter@Peter.com", "https://www.Peter.com"),
+        BEFriend("Anders", "87654321", "Anders@Anders.com", "https://www.Anders.com"),
+        BEFriend("Mikkel", "12121212", "Mikkel@Mikkel.com", "https://www.Mikkel.com"),
+        BEFriend("Flemming", "123", "Flemming@Flemming.com", "https://www.Flemming.com"),
+        BEFriend("Jonas", "1234", "Jonas@Jonas.com", "https://www.Jonas.com"),
+        BEFriend("Frederik", "12345", "Frederik@Frederik.com", "https://www.Frederik.com"),
+        BEFriend("Mantas", "12345678", "Mantas@Mantas.com", "https://www.Mantas.com"),
+        BEFriend("Michael", "23456789", "Michael@Michael.com", "https://www.Michael.com"),
+        BEFriend("Jens", "87654321", "Jens@Jens.com", "https://www.Jens.com"),
+        BEFriend("Jan", "12121212", "Jan@Jan.com", "https://www.Jan.com")
     )
 
     lateinit var friendsList: MutableList<BEFriend>
@@ -52,6 +52,8 @@ class Friends : Serializable{
     fun updateFriend(oldFriendIndex: Int, newFriend: BEFriend){
         friendsList[oldFriendIndex].name = newFriend.name
         friendsList[oldFriendIndex].phone = newFriend.phone
+        friendsList[oldFriendIndex].email = newFriend.email
+        friendsList[oldFriendIndex].url = newFriend.url
     }
 
 
